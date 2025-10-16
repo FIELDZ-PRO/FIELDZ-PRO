11c8a03d71227ebcd619bae762b37e186aca7996
Projects <nazimlameche1234@gmail.com>
2025-10-16 18:17:38 +0200

    feat: deconnect button added

--- frontend/src/components/organisms/HeaderClub.tsx
+++ frontend/src/components/organisms/HeaderClub.tsx
@@ -1,19 +1,33 @@
 import React from 'react';
 import { ChevronDown } from 'lucide-react';
 import './style/HeaderClub.css';
+import { useNavigate } from 'react-router-dom';
+
 
 const Header = () => {
+    const navigate = useNavigate();
+
+    const deconnectClub = () => {
+        localStorage.removeItem('token')
+        navigate("/LoginClub")
+    }
+
     return (
         <header className="header">
             <div className="header-content">
-                <div className="logo">
-                    <span className="logo-text">FieldZ</span>
-                </div>
-                <div className="header-right">
+                <div className="header-left">
+                    <div className="logo">
+                        <span className="logo-text">FieldZ</span>
+                    </div>
+
                     <div className="language-selector">
                         <span>Français</span>
                         <ChevronDown size={16} />
                     </div>
+                </div>
+
+                <div className="header-right">
+                    <button className="logout-button" onClick={deconnectClub}>Déconnexion</button>
                 </div>
             </div>
         </header>
